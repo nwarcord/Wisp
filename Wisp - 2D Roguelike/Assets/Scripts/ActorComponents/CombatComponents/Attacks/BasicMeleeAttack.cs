5c090e8b474dd195169e47d9abab78a56c58e0a7
Nathan Warren-Acord <nacord26@gmail.com>
2020-03-04 21:58:25 -0800

    Attack refactor continued

--- Wisp - 2D Roguelike/Assets/Scripts/ActorComponents/CombatComponents/Attacks/BasicMeleeAttack.cs
+++ Wisp - 2D Roguelike/Assets/Scripts/ActorComponents/CombatComponents/Attacks/BasicMeleeAttack.cs
@@ -2,25 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicMeleeAttack : Attack {
+// public class BasicMeleeAttack : Attack {
 
-    protected override void InitVariables() {
-        this.attackName = "Basic Melee";
-        this.damage = 1;
-        this.description = "A basic melee attack.";
-        this.range = 1;
+//     protected override void InitVariables() {
+//         this.attackName = "Basic Melee";
+//         this.damage = 1;
+//         this.description = "A basic melee attack.";
+//         this.range = 1;
+//     }
+
+//     public override bool ExecuteAttack(Vector3 tileCoords, int modifier) {
+    
+//         Transform actorPosition = gameObject.transform;
+//         Transform target = TileSystem.ObjectAtTile(tileCoords);
+//         if (target != null && TileSystem.TileDistance(target.position, actorPosition.position) <= range) {
+//             ICanBeDamaged victim = target.GetComponent<ICanBeDamaged>();
+//             if (victim != null) {
+//                 victim.TakeDamage(damage * modifier);
+//                 return true;
+//             }
+
+//         }
+//         return false;
+
+//     }
+
+// }
+
+public class BasicMeleeAttack {
+
+    private const int damage = 1;
+    private const int range = 1;
+    private Transform actorPosition;
+
+    public BasicMeleeAttack(Transform actorPosition) {
+        this.actorPosition = actorPosition;
     }
 
-    public override bool ExecuteAttack(Vector3 tileCoords, int modifier) {
+    public bool ExecuteAttack(Vector3 tileCoords, int dmgMod, int rangeMod) {
     
-        Transform actorPosition = gameObject.transform;
         Transform target = TileSystem.ObjectAtTile(tileCoords);
-        if (target != null && TileSystem.TileDistance(target.position, actorPosition.position) <= range) {
+        if (target != null && TileSystem.TileDistance(target.position, actorPosition.position) <= (range + rangeMod)) {
             ICanBeDamaged victim = target.GetComponent<ICanBeDamaged>();
             if (victim != null) {
-                victim.TakeDamage(damage * modifier);
+                victim.TakeDamage(damage + dmgMod);
                 return true;
-            }   
+            }
 
         }
         return false;
