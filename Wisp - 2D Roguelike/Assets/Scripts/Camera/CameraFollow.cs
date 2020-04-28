6f514a9ab9d114ce60db370bfb305f2cf7b6394d
Nathan Warren-Acord <nacord26@gmail.com>
2020-04-28 15:54:57 -0700

    Implemented a camera follow script and boundaries for clamping.

---
+++ Wisp - 2D Roguelike/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraFollow : MonoBehaviour {
+	//[SerializeField]
+	private Transform targetTransform;
+
+	[SerializeField]
+	private Camera thisCamera;
+
+	[SerializeField]
+	private Collider2D minBoundaryX, maxBoundaryX, minBoundaryY, maxBoundaryY;
+
+	private float xMin, xMax, yMin, yMax;
+
+	private float xOffset, yOffset;
+
+	private void Awake() {
+		if (GameObject.FindGameObjectsWithTag("MainCamera").Length > 1)
+			// Destroy any extra cameras.
+			Destroy(transform.parent.gameObject);
+
+		CustomHelpers.CheckForInitializedFields(this);
+
+		DontDestroyOnLoad(gameObject);
+		xOffset = thisCamera.orthographicSize * Screen.width / Screen.height;
+		yOffset = thisCamera.orthographicSize;
+
+		xMin = minBoundaryX.bounds.min.x + xOffset;
+		xMax = maxBoundaryX.bounds.max.x - xOffset;
+		yMin = minBoundaryY.bounds.min.y + yOffset;
+		yMax = maxBoundaryY.bounds.max.y - yOffset;
+	}
+
+	void Start() {
+
+        targetTransform = GameObject.FindWithTag("Player").transform;
+		//Debug.Log(targetTransform.position);
+    }
+
+    void LateUpdate() {
+		Follow();
+    }
+
+	private void Follow() {
+
+		Vector3 newPosition = new Vector3(targetTransform.position.x, targetTransform.position.y, transform.localPosition.z);
+
+		float xClamped = Mathf.Clamp(newPosition.x, xMin, xMax);
+		float yClamped = Mathf.Clamp(newPosition.y, yMin, yMax);
+
+		transform.localPosition = new Vector3(xClamped, yClamped, transform.localPosition.z);
+
+		// transform.localPosition = newPosition;
+	}
+}
--- Wisp - 2D Roguelike/Assets/Scripts/CustomTools/CustomHelpers.cs
+++ Wisp - 2D Roguelike/Assets/Scripts/CustomTools/CustomHelpers.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public static class CustomHelpers {
 
+    /// <summary>
+	/// Helper function for testing if Unity Object is destroyed or marked for destruction
+	/// </summary>
     public static bool IsNullOrDestroyed(this System.Object obj) {
             
         if (object.ReferenceEquals(obj, null)) return true;
@@ -12,4 +16,43 @@ public static class CustomHelpers {
 
         return false;
     }
+
+	/// <summary>
+	/// Helper function for this class to throw errors from the CheckForInitializedFields() function
+	/// </summary>
+	private static void ThrowInitError(string objectName, System.Type classObj, string gameObjectName) {
+#if UNITY_EDITOR
+		string message = string.Format("{0} not assigned in {1} on game object {2}", objectName, classObj.Name, gameObjectName);
+		Debug.LogError(message);
+		EditorApplication.isPlaying = false;
+#endif
+	}
+
+    public static void CheckForInitializedFields(UnityEngine.Object scriptToCheck) {
+
+#if UNITY_EDITOR
+		SerializedObject scriptSerialized = new SerializedObject(scriptToCheck);
+
+		SerializedProperty prop = scriptSerialized.GetIterator();
+
+		prop.NextVisible(true); // Need to pass the m_script component first.
+
+		while (prop.NextVisible(false)) {
+
+			//Debug.Log(prop.type);
+			if (prop.propertyType != SerializedPropertyType.ObjectReference) {
+				// Ignores built-in C# types like string, int, etc.
+				continue;
+			}
+			//Debug.Log(prop.propertyType);
+
+			if (!prop.objectReferenceValue) {
+				ThrowInitError(prop.name, scriptToCheck.GetType
+					(), scriptToCheck.name);
+			}
+		}
+#endif
+	}
+
+
 }
