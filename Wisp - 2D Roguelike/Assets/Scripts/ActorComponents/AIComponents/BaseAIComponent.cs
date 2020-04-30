cd2196a2e119e4baa40917f2a485e0b2ee4e140d
Nathan Warren-Acord <nacord26@gmail.com>
2020-04-30 12:41:49 -0700

    Added base AIComponent files

---
+++ Wisp - 2D Roguelike/Assets/Scripts/ActorComponents/AIComponents/BaseAIComponent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Enum for state choice? Patrol - Move - Attack (with type)
+
+public abstract class BaseAIComponent : MonoBehaviour {
+
+    protected Transform position;
+    protected Transform target;
+    protected Grid grid;
+
+    private void Awake() {
+        position = gameObject.transform;
+    }
+
+    private void Start() {
+        grid = GameState.grid;
+        SetTarget();
+    }
+
+    protected abstract void SetTarget();
+
+    // Non-combat behavior
+
+    // Combat behavior
+
+    // Choosing which state to move into
+
+    // If Patrol, decide number of tiles and direction
+
+    // If Move, decide number of tiles and direction (using target)
+
+    // If Attack, decide attack type
+
+}
