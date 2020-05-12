a26111b52872a6bd3f4296c894c1e94b858388e0
Nathan Warren-Acord <nacord26@gmail.com>
2020-05-12 15:52:32 -0700

    Finished non-tile-based ranged attacks. Aoe have their own layer.

--- Wisp - 2D Roguelike/Assets/Scripts/ActorComponents/CombatComponents/Attacks/RangedAttack.cs
+++ Wisp - 2D Roguelike/Assets/Scripts/ActorComponents/CombatComponents/Attacks/RangedAttack.cs
@@ -15,17 +15,28 @@ public class RangedAttack : IAttack {
     }
 
     public bool ExecuteAttack(Vector3 attackDirection) {
-    
+        
+        Vector3 mousePosition = attackDirection;
+        mousePosition.z = 0;
         Vector3 actorPos = actorPosition.position;
         actorPos.y -= 0.5f;
-        Vector3Int gridDirection = grid.WorldToCell(attackDirection);
-        Vector3Int actorTile = grid.WorldToCell(actorPosition.position);
-        actorTile.y -= 1;
-        gridDirection.z = actorTile.z;
-        Vector3 spawnPoint = TileSystem.AdjacentTile(gridDirection, actorTile, actorPosition.position);
-        spawnPoint.y -= 0.5f;
+        // Vector3Int gridDirection = grid.WorldToCell(attackDirection);
+        // Vector3Int actorTile = grid.WorldToCell(actorPosition.position);
+        // actorTile.y -= 1;
+        // gridDirection.z = actorTile.z;
+        // Vector3 spawnPoint = TileSystem.AdjacentTile(gridDirection, actorTile, actorPosition.position);
+        // spawnPoint.y -= 0.5f;
+        
+        // Vector3 spawnPoint = (attackDirection - actorPos).normalized;
+        // spawnPoint *= 3.5f;
+        // spawnPoint += actorPos;
+
+        Vector3 spawnPoint = (mousePosition - actorPos).normalized;
+        // spawnPoint *= 1.5f;
+        spawnPoint += actorPos;
 
         if (spawnPoint == actorPos) return false;
+        if (Vector3.Magnitude(mousePosition - actorPos) < 0.5f) return false;
 
         Vector3 rotatedVectorToTarget = Quaternion.Euler(0, 0, -45) * (spawnPoint - actorPos);
 
