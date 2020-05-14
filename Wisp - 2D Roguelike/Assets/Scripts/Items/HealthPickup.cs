e0ae8cc345f7eb6ccdd6c8736257dbf3034143b0
Nathan Warren-Acord <nacord26@gmail.com>
2020-05-14 11:37:29 -0700

    Added health pickups. New assets for alt enemies (stretch)

---
+++ Wisp - 2D Roguelike/Assets/Scripts/Items/HealthPickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour {
+
+    [SerializeField]
+    private int healValue = 0;
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            other.gameObject.GetComponent<PlayerController>().Heal(healValue);
+            Destroy(gameObject);
+        }
+    }
+
+}
--- Wisp - 2D Roguelike/Assets/Scripts/PlayerController.cs
+++ Wisp - 2D Roguelike/Assets/Scripts/PlayerController.cs
@@ -64,12 +64,19 @@ public class PlayerController : MonoBehaviour, ICanBeDamaged {
     public void TakeDamage(int damage) {
         Debug.Log("PLAYER DAMAGED | Health before: " + this.health + " and after: " + (this.health - damage));
         health -= damage;
-        EventManager.RaisePlayerDamaged();
+        if (health < 0) health = 0;
+        EventManager.RaisePlayerHealthUpdate();
         // Debug.Log("Player health: " + this.health + " | Damage taken: " + damage);
     }
 
     public bool IsAlive() {
         return health > 0;
+    }
+
+    public void Heal(int amount) {
+        this.health += amount;
+        if (this.health > 10) this.health = 10;
+        EventManager.RaisePlayerHealthUpdate();
     }
 
     // ----------------------------------------------------------------
