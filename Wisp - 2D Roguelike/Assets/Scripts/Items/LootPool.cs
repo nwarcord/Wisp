4f87a8de47053b4d9259c4cd132774f560f78655
Nathan Warren-Acord <nacord26@gmail.com>
2020-05-23 21:36:35 -0700

    Implemented enemy drops (just health for now)
    Enemies can drop GameObjects when they die.
    The only drops implemented right now are health pickups (common and uncommon)

---
+++ Wisp - 2D Roguelike/Assets/Scripts/Items/LootPool.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootPool : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject commonDrop = default;
+    [SerializeField]
+    private GameObject uncommonDrop = default;
+
+    private void OnDestroy() {
+        Vector3 spawnLocation = transform.position;
+        spawnLocation.z = 0;
+        System.Random rand = new System.Random();
+        int chance = rand.Next(0, 100);
+        if (chance < 30) GameObject.Instantiate(commonDrop, spawnLocation, new Quaternion());
+        else if (chance < 40) GameObject.Instantiate(uncommonDrop, spawnLocation, new Quaternion());
+    }
+
+}
