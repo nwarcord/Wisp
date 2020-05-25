c841e40f714562df00817cc2564202ee4f6024b7
Nathan Warren-Acord <nacord26@gmail.com>
2020-05-24 18:19:56 -0700

    Added setup for blocked paths.

---
+++ Wisp - 2D Roguelike/Assets/Scripts/GameState/PathBlock.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathBlock : MonoBehaviour {
+
+    [SerializeField]
+    private List<GameObject> blocks = default;
+
+    private void BlockPath(bool state) {
+        foreach(GameObject block in blocks) {
+            block.SetActive(state);
+        }
+    }
+
+
+}
